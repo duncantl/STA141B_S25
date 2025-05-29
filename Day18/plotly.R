930060e1d567a2f149c6c29eb5a41b03b5c40a81
Duncan Temple Lang <duncan@r-project.org>
2025-05-29 09:01:15 -0700

    create a plotly plot.

---
+++ Day18/plotly.R
@@ -0,0 +1,16 @@
+library(ggplot2)
+library(plotly)
+
+p <- ggplot(mpg, aes(x=displ, 
+                     y=hwy, 
+                     color=class,
+                     label1 = manufacturer,
+                     label2 = model,
+                     label3 = year)) +
+  geom_point(size=3) +
+  labs(x = "Engine displacement",
+       y = "Highway Mileage",
+       color = "Car Class") +
+  theme_bw()
+
+ggplotly(p, tooltip = c("label1", "label2", "label3"))
